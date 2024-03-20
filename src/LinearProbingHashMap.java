6f41a59bfefa2ff54d709e53a2616e893bfe2d17
Dean Hiromasa Miyata-Dawson <51884979+DeanHiromasaMiyata-Dawson@users.noreply.github.com>
2024-03-19 21:22:10 -0400

    Update LinearProbingHashMap.java

--- src/LinearProbingHashMap.java
+++ src/LinearProbingHashMap.java
@@ -5,47 +5,27 @@ import java.util.HashSet;
 import java.util.ArrayList;
 
 /**
- * Your implementation of a LinearProbingHashMap.
- *
- * @author Dean Hiromasa Miyata-Dawson
- * @version 1.0
- * @userid ddawson42
- * @GTID 903833148
- *
- * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
- *
- * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ * Implementation of a LinearProbingHashMap.
  */
-
-
-
 public class LinearProbingHashMap<K, V> {
 
     /**
      * The initial capacity of the LinearProbingHashMap when created with the
      * default constructor.
-     *
-     * DO NOT MODIFY THIS VARIABLE!
      */
     public static final int INITIAL_CAPACITY = 13;
 
     /**
      * The max load factor of the LinearProbingHashMap
-     *
-     * DO NOT MODIFY THIS VARIABLE!
      */
     public static final double MAX_LOAD_FACTOR = 0.67;
 
-    // Do not add new instance variables or modify existing ones.
     private LinearProbingMapEntry<K, V>[] table;
     private int size;
 
     /**
      * Constructs a new LinearProbingHashMap.
-     *
-     * The backing array should have an initial capacity of INITIAL_CAPACITY.
-     *
-     * Use constructor chaining.
+     * The backing array has initial capacity of INITIAL_CAPACITY.
      */
     public LinearProbingHashMap() {
         this(INITIAL_CAPACITY);
@@ -53,10 +33,8 @@ public class LinearProbingHashMap<K, V> {
 
     /**
      * Constructs a new LinearProbingHashMap.
-     *
-     * The backing array should have an initial capacity of initialCapacity.
-     *
-     * You may assume initialCapacity will always be positive.
+     * The backing array should has an initial capacity of initialCapacity.
+     * initialCapacity is assumed to be always positive.
      *
      * @param initialCapacity the initial capacity of the backing array
      */
@@ -73,29 +51,22 @@ public class LinearProbingHashMap<K, V> {
      * In the case of a collision, use linear probing as your resolution
      * strategy.
      *
-     * Before actually adding any data to the HashMap, you should check to
-     * see if the array would violate the max load factor if the data was
+     * Before actually adding any data to the HashMap,
+     * checks if the array would violate the max load factor if the data was
      * added. For example, let's say the array is of length 5 and the current
      * size is 3 (LF = 0.6). For this example, assume that no elements are
      * removed in between steps. If another entry is attempted to be added,
-     * before doing anything else, you should check whether (3 + 1) / 5 = 0.8
-     * is larger than the max LF. It is, so you would trigger a resize before
-     * you even attempt to add the data or figure out if it's a duplicate. Be
-     * careful to consider the differences between integer and double
-     * division when calculating load factor. Additionally, be very careful to
-     * use the correct types when comparing with LF. Comparing types with
-     * different precisions may result in unexpected rounding errors.
+     * before doing anything else, checks whether (3 + 1) / 5 = 0.8 is larger than the max LF.
+     * It is, so resizes before adding the data or figure out if it's a duplicate.
      *
-     * When regrowing, resize the length of the backing table to
-     * 2 * old length + 1. You must use the resizeBackingTable method to do so.
+     * Regrowing resizes the length of the backing table to 2 * old length + 1 using the resizeBackingTable method to do so.
      *
-     * Return null if the key was not already in the map. If it was in the map,
-     * return the old value associated with it.
+     * Returns null if the key was not already in the map.
+     * If it was in the map, returns the old value associated with it.
      *
      * @param key the key to add
      * @param value the value to add
-     * @return null if the key was not already in the map. If it was in the
-     * map, return the old value associated with it
+     * @return null if the key was not already in the map. If it was in the map, return the old value associated with it
      * @throws java.lang.IllegalArgumentException if key or value is null
      */
     public V put(K key, V value) {
@@ -182,7 +153,7 @@ public class LinearProbingHashMap<K, V> {
      * @param key the key to search for in the map
      * @return the value associated with the given key
      * @throws java.lang.IllegalArgumentException if key is null
-     * @throws java.util.NoSuchElementException   if the key is not in the map
+     * @throws java.util.NoSuchElementException if the key is not in the map
      */
     public V get(K key) {
         if (key == null) {
@@ -221,9 +192,7 @@ public class LinearProbingHashMap<K, V> {
     }
 
     /**
-     * Returns a Set view of the keys contained in this map.
-     *
-     * Use java.util.HashSet.
+     * Returns a Set view of the keys contained in this map using a HashSet.
      *
      * @return the set of keys in this map
      */
@@ -248,12 +217,10 @@ public class LinearProbingHashMap<K, V> {
     }
 
     /**
-     * Returns a List view of the values contained in this map.
+     * Returns a List view of the values contained in this map using an ArrayList.
      *
-     * Use java.util.ArrayList or java.util.LinkedList.
-     *
-     * You should iterate over the table in order of increasing index and add
-     * entries to the List in the order in which they are traversed.
+     * Iterates over the table in order of increasing index 
+     * and adds entries to the List in the order in which they are traversed.
      *
      * @return list of values in this map
      */
@@ -282,23 +249,15 @@ public class LinearProbingHashMap<K, V> {
      *
      * Disregard the load factor for this method. So, if the passed in length is
      * smaller than the current capacity, and this new length causes the table's
-     * load factor to exceed MAX_LOAD_FACTOR, you should still resize the table
-     * to the specified length and leave it at that capacity.
+     * load factor to exceed MAX_LOAD_FACTOR, resizes the table
+     * to the specified length and leaves it at that capacity.
      *
-     * You should iterate over the old table in order of increasing index and
-     * add entries to the new table in the order in which they are traversed.
-     * You should NOT copy over removed elements to the resized backing table.
-     *
-     * Since resizing the backing table is working with the non-duplicate
-     * data already in the table, you shouldn't explicitly check for
-     * duplicates.
-     *
-     * Hint: You cannot just simply copy the entries over to the new array.
+     * Iterates over the old table in order of increasing index and
+     * adds entries to the new table in the order in which they are traversed.
+     * Without copying over removed elements to the resized backing table.
      *
      * @param length new length of the backing table
-     * @throws java.lang.IllegalArgumentException if length is less than the
-     *                                            number of items in the hash
-     *                                            map
+     * @throws java.lang.IllegalArgumentException if length is less than the number of items in the hash map
      */
     public void resizeBackingTable(int length) {
         if (length <= 0 || length > size) {
@@ -332,26 +291,18 @@ public class LinearProbingHashMap<K, V> {
     /**
      * Returns the table of the map.
      *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
      * @return the table of the map
      */
     public LinearProbingMapEntry<K, V>[] getTable() {
-        // DO NOT MODIFY THIS METHOD!
         return table;
     }
 
     /**
      * Returns the size of the map.
      *
-     * For grading purposes only. You shouldn't need to use this method since
-     * you have direct access to the variable.
-     *
      * @return the size of the map
      */
     public int size() {
-        // DO NOT MODIFY THIS METHOD!
         return size;
     }
 }
